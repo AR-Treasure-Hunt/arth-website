9531e12bf72dd1ce9fe500922da4af7cca7105a6
Samir Ranjit <ranjit1224samir@gmail.com>
2025-12-10 16:26:30 +0545

    [+] edited 2023 section for mobile along with desktop as well as for some details and story

--- components/Story.tsx
+++ components/Story.tsx
@@ -5,19 +5,19 @@ export default function StoryPage() {
   return (
     <main className="w-full bg-[#FFFAF5] flex items-center">
       <div className="landing_container mx-auto px-6 py-16 w-full">
-        <div className="grid grid-cols-1 lg:grid-cols-12 gap-10 items-center">
-          
-          {/* LEFT COLUMN – HEADING + TEXT */}
-          <div className="lg:col-span-7">
-            <h1
+        <h1
               className="font-jersey text-[#F7A23B] text-8xl font-normal md:text-[5.5rem] leading-none"
               style={{ lineHeight: 0.9 }}
             >
               Story
             </h1>
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-10 items-center">
+          
+          {/* LEFT COLUMN – HEADING + TEXT */}
+          <div className="lg:col-span-7">
 
             <div className="mt-6 max-w-xl text-[#2E2B26]">
-              <p className="text-lg font-jersey leading-relaxed mb-3">
+              <p className="text-lg md:text-xl font-jersey leading-relaxed mb-3">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been.
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum similique dolorem dolores atque laudantium aut doloremque nihil veniam recusandae odio, sapiente, ut dicta reprehenderit molestiae, nisi consequuntur. Natus culpa est quae ea vero nesciunt delectus iusto rerum numquam. Eos quam iusto nobis, facere voluptas accusamus, minima odit excepturi, nisi modi quo voluptate ad unde? Expedita dicta esse molestiae debitis laboriosam incidunt temporibus quo unde, deserunt similique ut tempora saepe culpa voluptas hic veniam? Sequi illum iusto ab praesentium necessitatibus dolorem, nostrum, voluptatem explicabo eius modi veritatis maxime deserunt odio quo, eveniet architecto atque cupiditate ullam et commodi exercitationem iste! Provident!
